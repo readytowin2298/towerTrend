67fa638a4bafec27418e7652ff02786f3f5332fa
David Ames <readytowin2298@gmail.com>
2021-05-04 16:43:58 -0500

    getting basic express server setup

---
+++ app.js
@@ -0,0 +1,4 @@
+const Tower = require('./models/tower')
+const express = require('express');
+const app = express();
+const ExpressError = require("./helpers/expressError");
--- index.js
+++
@@ -1,7 +0,0 @@
-const Tower = require('./models/tower')
-
-
-Tower.genTowers().then((data) => {
-    console.log(data);
-    process.exit();
-})
