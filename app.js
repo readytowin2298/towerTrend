cefb1aa35853f9ef17f9b63526b915932c7c698e
David Ames <readytowin2298@gmail.com>
2021-05-05 15:49:15 -0500

    basic setup

--- app.js
+++ app.js
@@ -2,3 +2,29 @@ const Tower = require('./models/tower')
 const express = require('express');
 const app = express();
 const ExpressError = require("./helpers/expressError");
+
+
+const graphRoutes = require('./routes/graph.js');
+app.use('/graph', graphRoutes);
+
+/** 404 handler */
+
+app.use(function(req, res, next) {
+    const err = new ExpressError("Not Found", 404);
+  
+    // pass the error to the next piece of middleware
+    return next(err);
+  });
+  
+  /** general error handler */
+  
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+  
+    return res.json({
+      status: err.status,
+      message: err.message
+    });
+  });
+  
+  module.exports = app;
